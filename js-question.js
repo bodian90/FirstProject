416b6c11a199735dd37def5249b0fb030439bbbc
Bohdan Artiukhov <artuhov.bogdan@gmail.com>
2019-06-04 14:24:44 +0200

    first commit

---
+++ js-question.js
@@ -0,0 +1,18 @@
+'use stict'
+
+let name = prompt("What's your name?", ""),
+    lastname = prompt("What's you surename?",""),
+    age = +prompt("How old are you?",""),
+    petname = prompt("Your pet's name","");
+
+
+let information = {
+    name:name,
+    lastname:surname,
+    age:age,
+    petname:petname,
+
+};
+
+
+
